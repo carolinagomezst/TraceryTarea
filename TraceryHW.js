660094e34a28c37b56d135bca028e9c7fdefc312
carolinagomezst <47337761+carolinagomezst@users.noreply.github.com>
2019-02-04 17:17:40 -0500

    Add files via upload

---
+++ TraceryHW.js
@@ -0,0 +1,61 @@
+{
+	"name": [
+		"Dakota",
+		"Sarah",
+		"Carolina",
+		"Hannah",
+		"Jenna",
+		"Jaclyn"
+	],
+	"occupation": [
+		"professor",
+		"CEO",
+		"doctor",
+		"teacher",
+		"coach",
+		"pilot"
+	],
+	"always": [
+		"rarely",
+		"never",
+		"always",
+		"sometimes"
+	],
+	"expecting": [
+		"anticipating",
+		"drawing",
+		"imagining",
+		"walking"
+	],
+	"adjective": [
+		"awesome",
+		"cool",
+		"bad",
+		"exciting",
+		"amazing"
+	],
+	"object": [
+		"tree",
+		"pencil",
+		"swing",
+		"chair",
+		"lamp"
+	],
+	"Pronoun": [
+		"We",
+		"They"
+	],
+	"place": [
+		"beach",
+		"trip",
+		"vacation"
+	],
+	"description": [
+		"rad",
+		"groovy",
+		"beautiful"
+	],
+	"origin": [
+		"#name#, a #occupation#, was #always# #expecting# this #adjective# #object#. #Pronoun# #always# went on #adjective# adventures on the #description# #place#. "
+	]
+}
